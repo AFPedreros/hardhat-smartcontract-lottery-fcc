f86ec7da39c9a95685e5120eb8a57b81aa102933
AFPedreros <felipe.pedreros94@gmail.com>
2022-10-24 11:09:01 -0500

    exporting abi and contractAddresses

--- deploy/99-update-frontend.js
+++ deploy/99-update-frontend.js
@@ -1,4 +1,5 @@
-const { ethers } = require("hardhat")
+const { ethers, network } = require("hardhat")
+const fs = require("fs")
 
 const FRONTEND_ADDRESSES_FILE =
     "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
@@ -8,9 +9,28 @@ module.exports = async () => {
     if (process.env.UPDATE_FRONTEND) {
         console.log("Updating frontend...")
         updateContractAddresses()
+        updateAbi()
     }
+}
+
+const updateAbi = async () => {
+    const lottery = await ethers.getContract("Lottery")
+    fs.writeFileSync(FRONTEND_ABI_FILE, lottery.interface.format(ethers.utils.FormatTypes.json))
 }
 
 const updateContractAddresses = async () => {
     const lottery = await ethers.getContract("Lottery")
+    const chainId = network.config.chainId.toString()
+    const currentAddresses = JSON.parse(fs.readFileSync(FRONTEND_ADDRESSES_FILE, "utf8"))
+    if (chainId in currentAddresses) {
+        if (!currentAddresses[chainId].includes(lottery.address)) {
+            currentAddresses.push(lottery.address)
+        }
+    }
+    {
+        currentAddresses[chainId] = [lottery.address]
+    }
+    fs.writeFileSync(FRONTEND_ADDRESSES_FILE, JSON.stringify(currentAddresses))
 }
+
+module.exports.tags = ["all", "frontend"]
